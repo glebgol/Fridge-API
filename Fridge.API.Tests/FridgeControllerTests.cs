7373c87c0f32ed2c6ebc2d1b77151200daa254b0
Gleb Golovnev <glebgolovnev200@gmail.com>
2022-10-18 13:57:42 +0300

    Added unit tests for get by id, post http methods

--- Fridge.API.Tests/FridgeControllerTests.cs
+++ Fridge.API.Tests/FridgeControllerTests.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using Contracts.Interfaces;
+using Entities.DataTransferObjects;
+using Fridge.API.AutoMapperProfile;
 using Fridge.API.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
@@ -8,19 +10,185 @@ namespace Fridge.API.Tests
 {
     public class FridgeControllerTests
     {
-        private readonly Mock<IRepositoryManager> _mockRepo = new();
-        private readonly Mock<IMapper> _mockMapper = new();
-        private readonly Mock<ILoggerManager> _mockLogger = new();
+        private readonly Mock<IRepositoryManager> _mockRepo;
+        private readonly Mock<ILoggerManager> _mockLogger;
+        private readonly IMapper _mapper;
         private readonly FridgeController _fridgeController;
+
+        public FridgeControllerTests()
+        {
+            _mockRepo = new Mock<IRepositoryManager>();
+            _mockLogger = new Mock<ILoggerManager>();
+            if (_mapper == null)
+            {
+                var mappingConfig = new MapperConfiguration(mc =>
+                {
+                    mc.AddProfile(new MappingProfile());
+                });
+                IMapper mapper = mappingConfig.CreateMapper();
+                _mapper = mapper;
+            }
+        }
 
         [Fact]
         public void GetAllFridges_ReturnsOkObjectResult()
         {
+            // Assign
             _mockRepo.Setup(repo => repo.Fridges.GetAllFridges(false)).Returns(GetTestItems.Fridges);
-            var controller = new FridgeController(_mockRepo.Object, _mockMapper.Object, _mockLogger.Object);
 
+            var controller = new FridgeController(_mockRepo.Object, _mapper, _mockLogger.Object);
+
+            // Act
             var result = controller.GetAllFridges() as OkObjectResult;
+
+            // Assert
             Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void GetFridge_NotExisting_ReturnsNotFound()
+        {
+            // Assign
+            _mockRepo.Setup(repo => repo.Fridges.GetFridge(It.IsAny<Guid>()))
+                .Returns<Guid>(guid => GetTestItems.Fridges.FirstOrDefault(f => f.Id == guid));
+
+            var notExistingFridgeId = new Guid("646f7719-74a5-47af-a2b9-3e99665e0000");
+            var controller = new FridgeController(_mockRepo.Object, _mapper, _mockLogger.Object);
+
+            // Act
+            var result = controller.GetFridge(notExistingFridgeId);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public void GetFridge_Existing_ReturnsOkObjectResult()
+        {
+            // Assign
+            _mockRepo.Setup(repo => repo.Fridges.GetFridge(It.IsAny<Guid>()))
+                .Returns<Guid>(guid => GetTestItems.Fridges.FirstOrDefault(f => f.Id == guid));
+
+            var existingFridgeId = new Guid("dbfc4f30-8cc4-47e5-b543-08dab08812cc");
+            var controller = new FridgeController(_mockRepo.Object, _mapper, _mockLogger.Object);
+
+            // Act
+            var result = controller.GetFridge(existingFridgeId);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void PostFridge_NullFridge_ReturnsBadRequest()
+        {
+            // Assign
+            var controller = new FridgeController(_mockRepo.Object, _mapper, _mockLogger.Object);
+            var fridgeModelId = GetTestItems.ExistingFridgeModelId;
+
+            // Act
+            var result = controller.PostFridge(fridgeModelId, null);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public void PostFridge_NullFridgeModel_ReturnsNotFound()
+        {
+            // Assign
+            _mockRepo.Setup(repo => repo.FridgeModels.GetFridgeModel(It.IsAny<Guid>()))
+                .Returns<Guid>(guid => GetTestItems.FridgeModels.FirstOrDefault(fm => fm.Id == guid));
+
+            var controller = new FridgeController(_mockRepo.Object, _mapper, _mockLogger.Object);
+            var fridgeModelId = GetTestItems.NotExistingFridgeModelId;
+            var fridgeForCreation = GetTestItems.FridgeForCreation;
+
+            // Act
+            var result = controller.PostFridge(fridgeModelId, fridgeForCreation);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public void PostFridge_NotNullFridge_ReturnsCreatedAtRoute()
+        {
+            // Assign
+            var expectedFridge = new Entities.Models.Fridge();
+            var expectedFridgeDto = new FridgeDto();
+            var fridges = GetTestItems.Fridges.ToList();
+            int sizeBeforeCreating = fridges.Count;
+            _mockRepo.Setup(repo => repo.FridgeModels.GetFridgeModel(It.IsAny<Guid>()))
+                .Returns<Guid>(guid => GetTestItems.FridgeModels.FirstOrDefault(fm => fm.Id == guid));
+            _mockRepo.Setup(repo => repo.Fridges.CreateFridge(It.IsAny<Guid>(), It.IsNotNull<Entities.Models.Fridge>()))
+                .Callback((Guid fridgeModelId, Entities.Models.Fridge fridge) =>
+                {
+                    fridge.ModelId = fridgeModelId;
+                    fridges.Add(fridge);
+                })
+                .Verifiable();
+
+            var controller = new FridgeController(_mockRepo.Object, _mapper, _mockLogger.Object);
+            var fridgeModelId = GetTestItems.ExistingFridgeModelId;
+            var fridgeForCreation = GetTestItems.FridgeForCreation;
+
+            // Act
+            var result = controller.PostFridge(fridgeModelId, fridgeForCreation);
+
+            // Assert
+            Assert.IsType<CreatedAtRouteResult>(result);
+            Assert.Equal<int>(sizeBeforeCreating, fridges.Count - 1);
+        }
+
+        [Fact]
+        public void DeleteFridge_NotExisting_ReturnsNotFound()
+        {
+            // Assign
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void DeleteFridge_Existing_ReturnsNoContent()
+        {
+            // Assign
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void UpdateFridge_NullFridgeForUpdate_ReturnsBadRequest()
+        {
+            // Assign
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void UpdateFridge_NotExisting_ReturnsNotFound()
+        {
+            // Assign
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void UpdateFridge_Existing_NoContent()
+        {
+            // Assign
+
+            // Act
+
+            // Assert
         }
     }
 }
--- Fridge.API.Tests/GetTestItems.cs
+++ Fridge.API.Tests/GetTestItems.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using Entities.DataTransferObjects;
+using Entities.Models;
+
 namespace Fridge.API.Tests
 {
     static class GetTestItems
@@ -11,33 +13,86 @@ namespace Fridge.API.Tests
                 {
                     new Entities.Models.Fridge
                     {
-                        Id = new Guid(),
+                        Id = new Guid("dbfc4f30-8cc4-47e5-b543-08dab08812cc"),
                         Name = "Холодильник у бабушки",
                         OwnerName = "Бабушка и Дедушка",
-                        ModelId = new Guid(),
+                        ModelId = new Guid("8cbe08ff-a185-4c9f-0494-08daaee05b68"),
                     },
                     new Entities.Models.Fridge
                     {
-                        Id = new Guid(),
+                        Id = new Guid("5a9c69e9-7e67-4790-b544-08dab08812cc"),
                         Name = "На свалку",
                         OwnerName = "БОМЖ",
-                        ModelId = new Guid(),
+                        ModelId = new Guid("f10698f8-2d7b-4095-eb3d-08daaee09009"),
                     },
                     new Entities.Models.Fridge
                     {
-                        Id = new Guid(),
+                        Id = new Guid("46aad042-fa8d-49ea-b545-08dab08812cc"),
                         Name = "Дима",
                         OwnerName = "Домашний",
-                        ModelId = new Guid(),
+                        ModelId = new Guid("97e8aa83-ba12-4226-d1ab-08dab0876da4"),
                     },
                     new Entities.Models.Fridge
                     {
-                        Id = new Guid(),
+                        Id = new Guid("c6c8dcce-2c5b-44f4-b546-08dab08812cc"),
                         Name = "Оля",
                         OwnerName = "Дом2",
-                        ModelId = new Guid(),
+                        ModelId = new Guid("97e8aa83-ba12-4226-d1ab-08dab0876da4"),
                     }
                 };
+            }
+        }
+        public static FridgeForCreationDto FridgeForCreation
+        {
+            get
+            {
+                return new FridgeForCreationDto
+                {
+                    Name = "Холодильник у бабушки",
+                    OwnerName = "Бабушка и Дедушка",
+                };
+            }
+        }
+        public static IEnumerable<FridgeModel> FridgeModels
+        {
+            get
+            {
+                return new List<FridgeModel>()
+                {
+                    new FridgeModel
+                    {
+                        Id = new Guid("8cbe08ff-a185-4c9f-0494-08daaee05b68"),
+                        Name = "Atlant-2022",
+                        Year = 2022
+                    },
+                    new FridgeModel
+                    {
+                        Id = new Guid("97e8aa83-ba12-4226-d1ab-08dab0876da4"),
+                        Name = "LG",
+                        Year = 2018
+                    },
+                    new FridgeModel
+                    {
+                        Id = new Guid("19d6f605-7596-4cd9-d1ac-08dab0876da4"),
+                        Name = "Bosch",
+                        Year = 2022
+                    }
+                };
+            }
+        }
+
+        public static Guid ExistingFridgeModelId
+        {
+            get
+            {
+                return new Guid("8cbe08ff-a185-4c9f-0494-08daaee05b68");
+            }
+        }
+        public static Guid NotExistingFridgeModelId
+        {
+            get
+            {
+                return new Guid("87ea011e-6d52-42a3-b547-08dab08812cc");
             }
         }
     }
