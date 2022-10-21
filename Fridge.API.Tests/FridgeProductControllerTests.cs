7758731ffe9bb909d271783435ea1e49f12d62b2
Gleb Golovnev <glebgolovnev200@gmail.com>
2022-10-21 10:54:46 +0300

    Added FridgeProductController Tests

---
+++ Fridge.API.Tests/FridgeProductControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Contracts.Interfaces;
+using Fridge.API.AutoMapperProfile;
+using Moq;
+
+namespace Fridge.API.Tests
+{
+    public class FridgeProductControllerTests
+    {
+        private readonly Mock<IRepositoryManager> _mockRepo;
+        private readonly Mock<ILoggerManager> _mockLogger;
+        private readonly IMapper _mapper;
+
+        public FridgeProductControllerTests()
+        {
+            _mockRepo = new Mock<IRepositoryManager>();
+            _mockLogger = new Mock<ILoggerManager>();
+            if (_mapper == null)
+            {
+                var mappingConfig = new MapperConfiguration(mc =>
+                {
+                    mc.AddProfile(new MappingProfile());
+                });
+                IMapper mapper = mappingConfig.CreateMapper();
+                _mapper = mapper;
+            }
+            SetUpMock();
+        }
+
+        private void SetUpMock()
+        {
+
+        }
+    }
+}
